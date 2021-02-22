7c0640ce37348d3be1f7cf6dcc104bbcc08fbaac
Emre Engin <53485321+enginemre@users.noreply.github.com>
2021-02-22 21:25:35 +0300

    Update ConfigData.java

--- app/src/main/java/com/engin/eticaretkontrol/ConfigData.java
+++ app/src/main/java/com/engin/eticaretkontrol/ConfigData.java
@@ -10,8 +10,8 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public  class ConfigData {
-    public static final String  CLIENT_ID ="8_4jpv8zhavp4wgso08g04cs408k0wcksw4ccsowsg0o4cooks0o";
-    public static final String CLIENT_SECRET = "1io8il2swexw8wkk4s0swwcosko04ws8gk4ccsw8sosco8gk4s";
+    public static final String  CLIENT_ID ="bu8_4jpv8zhavp4wgso08g04csdeneme408k0wcksw4ccsowsg0o4cooks0o";
+    public static final String CLIENT_SECRET = "bu1io8il2swexwgizli8wkk4s0swwcosko04ws8gk4ccsw8sosco8gk4s";
     public static final String REDIRECT_URI = "http://www.hakmarexpress.com/api-key-hakmar2.php";
     public static final String RESPONSE_TYPE = "code";
     public static final String STATE="1233fdd45jbevd6nam";
